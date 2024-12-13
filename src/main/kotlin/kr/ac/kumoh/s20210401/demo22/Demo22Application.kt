c012329eb905ed24b58ddeb735a8cbcad24279b0
Ryu Tae Hyun <ryu200112@gmail.com>
2024-12-13 16:24:07 +0900

    Initial commit

---
+++ src/main/kotlin/kr/ac/kumoh/s20210401/demo22/Demo22Application.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20210401.demo22
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Demo22Application
+
+fun main(args: Array<String>) {
+	runApplication<Demo22Application>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20210401/demo22/Demo22ApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20210401.demo22
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Demo22ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
